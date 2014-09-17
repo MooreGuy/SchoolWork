1f949e2f444265336ad92eb342a0bd736817f150
Guy Moore <guy.moore.normal@gmail.com>
2014-09-17 11:08:39 -0700

    Added the application compatibility and the experimental branch.

---
+++ Cat/src/com/guywmoore/cat/SettingsFragment.java
@@ -0,0 +1,11 @@
+package com.guywmoore.cat;
+
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+public class SettingsFragment extends Fragment
+{
+	@Override
+	public View onCreateView(LayoutInflater infalter, ViewGroup container,
+							 Bundle savedIntanceState)
+}
