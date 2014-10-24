0eb9869a0435f99ac8116d72fe6a86a7740205d2
Guy Moore <guy.moore.normal@gmail.com>
2014-10-23 23:51:36 -0700

    Changed the array size to the maximum possilbe size, since it is risky setting it as a variable size.
    The input functionality is now completely finished, and now all that needs to be worked on is output and output logic.
    
    Added getchar()'s to the beginning of each scanf() to avoid an infinite loop of it checking the input register for an invalid character.

--- cis231/C/Assignment3/main.c
+++ cis231/C/Assignment3/main.c
@@ -7,20 +7,31 @@
 
 #include <stdio.h>
 
+#define TEMP_ARRAY_SIZE 40
+
 void getNumber(int * numTemps);
 int getTemperature(double temps[], int numTemps);
+int getTemps(double temps[], int numTemps);
+void printHeader();
+void printSpaceing(int space);
 void printTemps(const double temps[], int arraySize);
+double convertFahrToCels(double fahrTemp);
 
 int main(void)
 {
-    int numTemps;
+    int numTemps; 
     //TODO: Ask Randy if declaring arrays with variables
     // is good practice 
-    double temps[numTemps];
+    double temps[TEMP_ARRAY_SIZE];
 
     getNumber(&numTemps);
     getTemps(temps, numTemps);
+
+    printHeader();
     printTemps(temps, numTemps);
+
+    //Testing the Fahrenheit to Celsius conversion function
+    printf("Converted %lf to %lf\n", 32.0, convertFahrToCels(32.0));
 
     return 0;
 }
@@ -47,14 +58,14 @@ void getNumber(int * numTemps)
 
 int getTemps(double temps[], int numTemps)
 {
-    printf("Enter a Fahrenheit temperature from 1 to 40 "
-           "(inclusive).\n");
+    printf("Now enter Fahrenheit temperatures from -200.0 to 300.0"
+           "(.\n");
 
     int i;
     for ( i = 0; i < numTemps; i++)
     {
         printf("Please enter a value for temperature %i.\n", i);
-	scanf("%lf", temps[i]);
+	scanf("%lf", &temps[i]);
         
 	//Check for valid input, and request a new number if invalid
 	while(!(temps[i] >= -200.0) || !(temps[i] <= 300.0))
@@ -64,12 +75,54 @@ int getTemps(double temps[], int numTemps)
 		   " -200.0 and 300.0 Fahrenheit.\n");
 	    printf("\nEnter a valid temperature\n");
 	    
+	    getchar(); 
 	    scanf("%lf", temps[i]); 
 	}
     }
 }
 
-void printTemps(const double temps[], int arraySize)
+/* 
+ *  Prints the project header with the assignment name and then my name
+ */
+void printHeader()
+{
+    printf("Assignment 3 | Guy Moore\n");
+}
+
+/*
+ *  Prints a specified number of spaces to format the document
+ */
+void printSpacing(int space)
 {
     int i;
+    for( i = 0; i < space; i++)
+    {
+	printf(" ");
+    }
+}
+
+/*
+ *  Prints the temperatures in both Fahrenheit and Celsius units.
+ */
+void printTemps(const double temps[], int arraySize)
+{
+    printSpacing(20);
+    printf("Fahr");
+    printSpacing(10);
+    printf("Cels\n");    
+
+    int i;
+    for( i = 0; i < arraySize; i++)
+    {
+	printSpacing(20);
+	printf("%-.2lf", temps[i]);
+	printSpacing(10);
+	printf("%.2lf", convertFahrToCels(temps[i]));
+	printf("\n");
+    }	
+}
+
+double convertFahrToCels(double fahrTemp)
+{
+    return (fahrTemp - 32) * 5 / 9;
 }
