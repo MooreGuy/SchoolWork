507b5f675ba4b67e0ceb88323bf6394b44b69cf6
3413 <3413student@lab-3413-17.cuesta.org>
2014-10-28 13:53:40 -0700

    Added lab13

---
+++ cis231/C/Lab13/main.c
@@ -0,0 +1,27 @@
+/*
+ *	Guy Moore
+ *	Lab13 | CIS 231 B
+ *	Cuesta College | Randy Scovil
+ *	Due: 12:00 30/10/2014
+ */
+
+#define ARRAY_LENGTH
+
+void getString( char string[], ARRAY_LENGTH);
+
+
+int main(int argc, char * argv)
+{
+	//Variables
+	char string[ARRAY_LENGTH];		
+	
+	return 0;
+}
+
+void getString( char string[], ARRAY_LENGTH)
+{
+	int i;	
+}	
+
+
+
