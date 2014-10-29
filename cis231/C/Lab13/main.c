b40c19ea4f9037f1826b05034c51fd50511f357f
Guy Moore <guy.moore.normal@gmail.com>
2014-10-29 14:24:33 -0700

    Made function for getting the string, stops when user enters a newline. Added the checkString() prototype, it will check if the string meats the requirements.

--- cis231/C/Lab13/main.c
+++ cis231/C/Lab13/main.c
@@ -8,19 +8,49 @@
 #include <stdio.h>
 
 #define ARRAY_LENGTH 81
+#define NUM_OF_STRINGS 5
 
-void getString( char string[], int arraySize);
+void getString( char string[] );
 
+int checkString( char  string[],  int * combinedAlphabetic, 
+				int * combinedLowerCase, int * combinedNumeric, 
+				int * combinedAlphanumeric, int * combinedOther );
 
 int main(int argc, char * argv)
 {
 	//Variables
-	char string[ARRAY_LENGTH];		
-	
+	char string[ARRAY_LENGTH];	
+
+	int combinedAlphabetic = 0;	
+	int combinedLowerCase = 0;
+	int combinedNumeric = 0;
+	int combinedAlphanumeric = 0;
+	int combinedOther = 0;
+
+	//Loop stringChecker	
+	int i;
+	for( i = 0; i <= NUM_OF_STRINGS; i++ )
+	{
+		checkString( string, &combinedAlphabetic, &combinedLowerCase,
+			 &combinedNumeric, &combinedAlphanumeric, &combinedOther );
+	}	
 	return 0;
 }
 
-void getString( char string[], int arraySize)
+void getString( char string[] )
 {
-	int i;	
+	int i = 0;
+	while((string[i] = getchar()) != '\n' && i < ARRAY_LENGTH)
+	{
+		i++;
+	}	
+	string[i] = '\0';
 }
+
+
+int checkString( char string[], int * combinedAlphabetic,
+					int * combinedLowerCase, int * combinedNumeric,
+					int * combinedAlphanumeric, int * combinedOther)
+{
+	return 0;
+}	
