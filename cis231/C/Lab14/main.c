3b12e9daeb41db9a22e09f792eea21f300456dcc
Guy Moore <guy.moore.normal@gmail.com>
2014-11-03 00:01:17 -0800

    Added string input, but input doesn't clear previous input.

--- cis231/C/Lab14/main.c
+++ cis231/C/Lab14/main.c
@@ -7,7 +7,27 @@
 
 #include <stdio.h>
 
+void strInput(char str[], int maxChars);
+
 int main(int argc, char * argv)
 {
+	char str[80];
+	strInput(str, 10);
+	printf("Input: %s\n", str);
+}
 
+void strInput(char str[], int maxChars)
+{
+	//Clear buffer.
+	while( getchar() != '\0');
+
+	//Get input.
+	int i = 0;
+	char currentCharacter;
+	while( ( currentCharacter = getchar() ) != '\n' && i <= maxChars )
+	{
+		str[i] = currentCharacter;
+		i++;
+	}	
+	str[i + 1] = '\0';
 }
