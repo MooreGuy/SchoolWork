21737aed511dfbbb517bfc99d89d7a94921e3842
Guy Moore <guy.moore.normal@gmail.com>
2014-11-22 20:18:18 -0800

    Added the method to print an array with a specified numbers per line. Added comments to both this method and the random arrray generator method

--- cis231/Java/Lab17/src/Lab17GMoo.java
+++ cis231/Java/Lab17/src/Lab17GMoo.java
@@ -12,8 +12,17 @@ public class Lab17GMoo
 		System.out.println(myNumbers[0]);
 			
 	}
-	
-	public static int [] randomNumbers( int size )
+
+	/* Takes in a integer parameter and returns an integer array filled
+	 * with randomly created values the size of the parameter.
+	 * 
+	 * First creates the array, then creates a Random object to create the
+	 * random numbers to fill the array.
+	 * Next, it loops through the array adding randomly generated numbers
+	 * to each element
+	 * Finaly, the method returns the arary.
+	 */	
+	public static int[] randomNumbers( int size )
 	{
 		int [] numbers = new int[size];
 		Random gen = new Random();	
@@ -24,5 +33,28 @@ public class Lab17GMoo
 		}
 	
 		return numbers;		
+	}
+
+	/* 
+	 * Prints the out the array numbers, with each line having as many
+	 * numbers as the parameter numPerLine.
+	 *
+	 * Loops through first loop to keep track of the entire array, and
+	 * then the second loop keeps track of the numbers in the current line.
+	 */
+	public static void printArray( int[] numbers, int numPerLine )
+	{
+		//Goes through entire array..
+		for( int i = 0; i < numbers.length; i++ )
+		{
+			//Keeps track of how many numbers should be printed per line.
+			for( int k = 0; k < numPerLine; k++ )
+			{
+				System.out.print(numbers[i] + " ");
+			} 
+			
+			//Return to next line.	
+			System.out.println("");
+		}
 	}
 }
