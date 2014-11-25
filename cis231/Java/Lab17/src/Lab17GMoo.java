6b6e23b7520ca5af92c701ee79b5b438f45ac395
Guy Moore <guy.moore.normal@gmail.com>
2014-11-25 10:43:46 -0800

    Fixed spacing

--- cis231/Java/Lab17/src/Lab17GMoo.java
+++ cis231/Java/Lab17/src/Lab17GMoo.java
@@ -6,7 +6,7 @@
  */
 
 
-import java.util.*;
+import java.util.Random;
 
 public class Lab17GMoo
 {
@@ -17,7 +17,9 @@ public class Lab17GMoo
 		System.out.println("Guy Moore");
 		
 		myNumbers = randomNumbers(10); 
+
 		printArray( myNumbers, 2 );
+
 		System.out.println( "\nThe largest number was: " + 
 			getLargest( myNumbers ) );
 			
