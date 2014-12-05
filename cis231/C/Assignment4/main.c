867249cfce9c3d3e435e5384b4e6b3898ff4d553
Guy Moore <guy.moore.normal@gmail.com>
2014-12-05 13:29:54 -0800

    Added improved string input from Lab15

--- cis231/C/Assignment4/main.c
+++ cis231/C/Assignment4/main.c
@@ -1,6 +1,28 @@
+/*
+ *	Guy Moore
+ *	Assignment 4 | CIS 231 B
+ *	Cuesta College | Randy Scovil
+ *	Due: 12/9 start of class
+ */
+
 #include <stdio.h>
+
+
+void strInput(char str[], int maxChars);
+
 int main()
 {
 
 	return 0;
+}
+
+void strInput(char str[], int maxChars)
+{
+	//Get input.
+	int i = 0;
+	while( i <= maxChars && ( str[i]  = getchar() ) != '\n' )
+	{
+		i++;
+	}	
+	str[i] = '\0';
 }
