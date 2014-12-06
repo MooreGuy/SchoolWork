7fd0f2c37e1852b77414a2b1e7b72d26adc9dfec
Guy Moore <guy.moore.normal@gmail.com>
2014-12-05 16:34:30 -0800

    Finished string input, halfway through tokenizing.

--- cis231/C/Assignment4/main.c
+++ cis231/C/Assignment4/main.c
@@ -7,11 +7,22 @@
 
 #include <stdio.h>
 
+#define STRING_LENGTH 81
+
+struct temp 
+{
+	int temperature;
+	temp * next;
+};
 
 void strInput(char str[], int maxChars);
+int * temps getTemps(  );
+void promptStrInp( char[] str );
+int * getTokens( char[] str );
 
 int main()
 {
+
 
 	return 0;
 }
@@ -26,3 +37,49 @@ void strInput(char str[], int maxChars)
 	}	
 	str[i] = '\0';
 }
+
+void getTemps()
+{
+	//String with defined string length to hold integer temperatures.
+	char tempString[STRING_LENGTH];
+
+	int 
+	//Size of the integer temp string;
+	int tempSize = 10;
+
+	//Get the first input.
+	promptStrInp( tempString );	
+
+	//Continue to get input until user enters an empty string
+	while ( tempString[0] != '\0' )
+	{		
+		//Tokens
+		getTokens( tempString, &tempSize );
+	
+		//Get string with prompted input.
+		promptStrInp( tempString );	
+	}
+	
+}
+
+void promptStrInp( char[] str )
+{	
+	//Prompt input on the same line it is to be entered.
+	printf("Please enter a value:", i);
+
+	//Get string input.
+	strInput( myString, STRING_LENGTH );	
+	
+	//Seperate input lines
+	printf("\n");
+}
+
+/*
+ * Tokenize the string and add it to the other inputed temperatures.
+ *
+ * Create a token, then convert it to an int. Put this int in to the
+ * temperatures.
+ */
+struct temp * getTokens( char[] str )
+{
+	
