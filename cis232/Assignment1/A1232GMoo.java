5ce87fca634e7ccd4ad6945699d630cc4b687bbd
Guy Moore <guy.moore.normal@gmail.com>
2015-09-20 20:57:02 -0700

    Add shell system or handling user input
    Because the options for this program are so limited, it makes more sense
    to make a simple system of if/else statements rather than create a
    complex shell program.
    
    All features are operating but the list function doesn't properly sort
    the Shapes. This is so I can focus on getting the shell to work.

--- cis232/Assignment1/A1232GMoo.java
+++ cis232/Assignment1/A1232GMoo.java
@@ -7,11 +7,77 @@
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 
 public class A1232GMoo
 {
+	private static final String[] options = {
+		"quit",
+		"create shape",
+		"list shapes"
+	};
+	private static ArrayList<Shape> shapes = new ArrayList<Shape>();
+
 	public static void main(String[] args)
 	{
+		// The program exits through System.exit, so this works.
+		while(true) {
+			printMenu();
+			handleOption(getInput("Select an option: "));
+		}
+	}
+
+	public static void printMenu()
+	{
+		for(String option : options)
+		{
+			System.out.println(option);
+		}
+	}
+
+	private static void handleOption(String selectedOption)
+	{
+		if(selectedOption.equals("quit")) {
+			System.exit(0);
+		}
+		else if(selectedOption.equals("create shape"))
+		{
+			String input = getInput("Enter shape type: ");
+
+			Shape createdShape = createShape(input);
+
+			// Print out the shape and add it to the shapes list.
+			System.out.println(createdShape.toString());
+			shapes.add(createdShape);
+		}
+		else if(selectedOption.equals("list shapes"))
+		{
+			boolean validInput = false;
+			do
+			{
+				String input = getInput(
+					"Enter sort method, ascending or descending: ");
+				if(input.equals("ascending"))
+				{
+					listShapes(shapes);
+					validInput = true;
+				}
+				else if(input.equals("descending"))
+				{
+					listShapes(shapes);
+					validInput = true;
+				}
+			}
+			while(validInput == false);
+		}
+	}
+
+	public static void listShapes(List<Shape> shapes)
+	{
+		for(Shape shape : shapes)
+		{
+			System.out.println(shape.toString());
+		}
 	}
 
 	public static double getLength(String prompt)
