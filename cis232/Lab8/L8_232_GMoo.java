29fbd75a33f34bdd46762df241e2a558a074381b
Guy Moore <guy.moore.normal@gmail.com>
2015-09-30 11:20:55 -0700

    Add recursive pdf search and test
    Takes a starting directory and recursively checks for more
    pdfs or directories.

--- cis232/Lab8/L8_232_GMoo.java
+++ cis232/Lab8/L8_232_GMoo.java
@@ -5,16 +5,26 @@
  * Instructor: Randy Scovil
  */
 import java.util.Stack;
+import java.util.ArrayList;
+import java.io.File;
+import javax.swing.JFileChooser;
 
 public class L8_232_GMoo {
 	public static void main(String[] args) {
 		for (int x = 1; x < 13; x++) {
 			System.out.println("Factorial " + x + ": " + factorial(x));
 		}
+
+		File startDirectory = chooseDirectory();
+		ArrayList<String> pdfPathNames = recursivePDFSpider(startDirectory);
+		for (String name : pdfPathNames) {
+			System.out.println(name);
+		}
 	}
 
 	public static int factorial(int base) {
 		Stack<Integer> factorialStack = new Stack<Integer>();
+
 		for (; base > 1; base--) {
 			factorialStack.push(base);
 		}
@@ -25,5 +35,32 @@ public class L8_232_GMoo {
 		}
 
 		return factorial;
+	}
+
+	public static File chooseDirectory() {
+		JFileChooser jfc = new JFileChooser();
+
+		jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+		if (jfc.showOpenDialog(null) != JFileChooser.APPROVE_OPTION) {
+			System.exit(0);
+		}
+
+		return jfc.getSelectedFile();
+	}
+
+	public static ArrayList<String> recursivePDFSpider(File startDirectory) {
+		ArrayList<String> pdfPathNames = new ArrayList<String>();
+
+		File[] list = startDirectory.listFiles();
+		for (File curFile : list) {
+			if (curFile.isDirectory()) {
+				pdfPathNames.addAll(recursivePDFSpider(curFile));
+			} else if(curFile.toPath().toString().endsWith(".pdf")) {
+				pdfPathNames.add(curFile.getAbsolutePath());
+			}
+		}
+
+		return pdfPathNames;
 	}
 }
