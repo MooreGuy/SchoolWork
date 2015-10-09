d7e91ce5604ddf906353a6a631ced28e463c6800
Guy Moore <guy.moore.normal@gmail.com>
2015-10-09 13:14:56 -0700

    Add more tests.

--- cis232/Assignment2/cis232/a2/Main.java
+++ cis232/Assignment2/cis232/a2/Main.java
@@ -15,14 +15,57 @@ public class Main {
 		printCollection(myArrayList);
 		myArrayList.add("c");
 		printCollection(myArrayList);
-		myArrayList.add("c");	
+		myArrayList.add("c");
 		printCollection(myArrayList);
-		myArrayList.add(4, "c");	
+		myArrayList.add(4, "c");
 		printCollection(myArrayList);
 		System.out.println(myArrayList.set(3, "z") + "\n");
 		printCollection(myArrayList);
 
 		System.out.println(A2232GMoo.binarySearch(myArrayList, "c"));
+
+		myArrayList.add("0");
+		printCollection(myArrayList);
+		myArrayList.add("e");
+		myArrayList.add("e");
+		myArrayList.add("e");
+		myArrayList.add("c");
+		myArrayList.add("c");
+		myArrayList.add("c");
+		myArrayList.add("0");
+		myArrayList.add("0");
+		myArrayList.add("0");
+		myArrayList.add("0");
+		myArrayList.add("0");
+		myArrayList.add("0");
+		myArrayList.add("e");
+		myArrayList.add("e");
+		myArrayList.add("e");
+		myArrayList.add("e");
+		printCollection(myArrayList);
+
+		Result<String> myResult = myArrayList.getMode();
+		System.out.println("mode: " + myResult.mode() + ", count: " +
+				myResult.count());
+		printCollection(myArrayList);
+
+		myArrayList.add(null);
+		myArrayList.add(null);
+		myArrayList.add(null);
+		myArrayList.add(null);
+		myArrayList.add(null);
+		myArrayList.add(null);
+		myArrayList.add(null);
+		myArrayList.set(5, null);
+		myArrayList.set(5, null);
+		myArrayList.set(5, null);
+
+
+		printCollection(myArrayList);
+
+		myResult = myArrayList.getMode();
+		System.out.println("mode: " + myResult.mode() + ", count: " +
+				myResult.count());
 	}
 
 	public static <AnyType> void printCollection(
